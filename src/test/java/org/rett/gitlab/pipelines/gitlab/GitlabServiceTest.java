e090b978fe45e7307f90353513bf1b60bf41a039
Garrett <garrphelps@gmail.com>
2023-10-14 14:37:43 -0500

    Upgrade to java 17

--- src/test/java/org/rett/gitlab/pipelines/gitlab/GitlabServiceTest.java
+++ src/test/java/org/rett/gitlab/pipelines/gitlab/GitlabServiceTest.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public class GitlabServiceTest extends TestCase {
 
     public void testGetProjectPathFromRemote() throws Exception {
-        final Optional<HostAndProjectPath> optional = GitlabService.getHostProjectPathFromRemote("https://selfthosted.com/gitlab/project/intellij_gitlab_pipeline_monitor.git");
+        final Optional<HostAndProjectPath> optional = GitlabService.getHostProjectPathFromRemote("https://gitlab.com/Rett/test.git");
         System.out.println(optional.get());
 //        final Optional<GitlabService.HostAndProjectPath> projectPathFromRemote = GitlabService.getProjectPathFromRemote("https://gitlab.ppi.int/tph/prod/phmaven.git");
         System.out.println(Jackson.OBJECT_MAPPER.writeValueAsString("query {\n" +
